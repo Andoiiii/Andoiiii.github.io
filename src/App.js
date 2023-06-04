9a85ec54a26a7b98d032c018e5d0ca146ef774f7
Andy Chang (Andoiii) <andychang268@gmail.com>
2023-06-03 19:31:58 -0700

    Starting on this project - Added, what, a slide counter scrolly component - removed previous react app defaults

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { SlideCounter } from './assets/SlidingCounter';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hey! Andoiii is busy at work.</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          He had a next react nightmare.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Hi! I'm an Andoiii and I am your local
+      <SlideCounter />
+    </>
   );
 }
 
---
+++ src/assets/SlidingCounter.js
@@ -0,0 +1,51 @@
+import styles from "./SlidingCounter.module.css"
+import { useState, useEffect } from "react";
+//goal: a counter-esque object that displays different strings
+//and scrolls to different ones
+
+let choiceArr = [
+  "Software Dev in Training",
+  "Math Person",
+  "University Student",
+  "Madman",
+  "Dragon",
+  "Math Tutor",
+  "CS/BBA Student",
+  "Friend :)",
+  "Nerd",
+  "Bad Artist",
+  "Archivist"
+];
+let maxLen = choiceArr.reduce((acc,x) => Math.max(x.length, acc), 0);
+
+function SlideCounter() {
+  const [ currEntry, setCurrEntry ] = useState(0);
+  const fontSize = 16;
+  const fontHangSize = 10;
+
+  //go to a new, distinct entry every __ seconds
+  useEffect( () => {
+    const tickOver = setInterval(() => {
+      let x = currEntry;
+      while (x === currEntry) { x = Math.floor(Math.random() * choiceArr.length)}
+      setCurrEntry(x);
+    }, 3000);
+    return () => clearInterval(tickOver);
+  }, [currEntry])  
+
+
+  const fontStyles = { "--font-height": fontSize + "px", "--maxlen": maxLen, "--font-hang-size": fontHangSize + "px" };
+  const slideStyle = { "marginTop": (-1 * (currEntry * (fontHangSize + fontSize)))}
+
+  return (
+    <>
+      <div style={fontStyles} className={styles.Counter}>
+        <div className={styles.Entries} style={slideStyle}>
+          {choiceArr.map((x,ind) => <div className={styles.Entry} key={ind}><p>{x}</p></div>)}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export { SlideCounter };
