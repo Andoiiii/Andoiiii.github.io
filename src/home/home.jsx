b79dd455bf8673d56606847cf36561904b3868d6
Andy Chang (Andoiii) <andychang268@gmail.com>
2024-05-06 20:28:21 -0400

    I think the theme is up. had to use react antics. and referencing online but of course :)

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
+import { ThemeContext } from "../layout/theme/themeProvider";
+
+import home from './home.module.css'
+
 export default function Homepage() {
+    const setThemeFn = useContext(ThemeContext);
+
     return (
         <>
             <h1>Hello World!</h1>
+            {["light", "dark", "grayscale", "neon"].map((str) => {
+                return (
+                    <button key={str} onClick={() => setThemeFn(str)}>
+                        Set Theme to {str}
+                    </button>
+                );
+            })}
+            <div className={home.text}>
+                <button onClick={() => {}} className={home.primaryBg}>Primary Button</button>
+                <button onClick={() => {}} className={home.secondaryBg}>Secondary Button</button>
+                <p>This is some <span className={home.accent}>ACCENTED</span> text</p>
+            </div>
         </>
-    )
+    );
 }
